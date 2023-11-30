6c6e47d58bf8bfbfa04eb2e4425789e9385b9bee
“Carlos <carlosdevelopment@hotmail.com>
2023-11-29 18:30:16 -0600

    Add error handling & pipes

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 import { Country } from '../interfaces/countries.interface';
 
 @Injectable({
@@ -12,6 +12,18 @@ export class CountriesService {
   constructor(private httpClient: HttpClient) { }
 
   searchCapital(query: string) : Observable<Country[]> {
-    return this.httpClient.get<Country[]>(`${this.url}/capital/${query}`);
+    return this.httpClient.get<Country[]>(`${this.url}/capital/${query}`)
+      .pipe(
+        catchError(error => {
+          console.log(error);
+          return of([])
+        }),
+        tap(countries => console.log('Running tap')),
+        map(countries =>
+        {
+          console.log('Running map')
+          return countries;
+        }),
+      );
   }
 }
