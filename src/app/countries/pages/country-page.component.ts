2830183f0c18943c0aab4abede88bba90b8cd409
“Carlos <carlosdevelopment@hotmail.com>
2023-11-30 14:32:53 -0600

    Subscribe to url parameters

--- src/app/countries/pages/country-page.component.ts
+++ src/app/countries/pages/country-page.component.ts
@@ -1,9 +1,39 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { CountriesService } from "../services/countries.service";
 
 @Component({
   selector: 'country-page',
-  template: ``,
+  templateUrl: './country-page.component.html',
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
 
+  constructor(
+    private activatedRout: ActivatedRoute,
+    private countriesService: CountriesService) {
+  }
+
+  ngOnInit(): void {
+
+    /* Other option
+
+    this.activatedRout.params
+      .subscribe(urlParams => {
+        const idFromUrl = urlParams['id'];
+        this.countriesService.searchByAlpha(idFromUrl)
+          .subscribe(c => {
+
+          })
+      });
+
+    */
+
+    this.activatedRout.params
+      .subscribe(({ id }) => {
+        this.countriesService.searchByAlpha(id)
+          .subscribe(c => {
+            console.log(c);
+          })
+      });
+  }
 }
